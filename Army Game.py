c4031fe1c203dd19236cb319a3e400a4b11c1dc1
Md Easin Hasan <mhasan8@miners.utep.edu>
2021-11-01 20:38:27 -0600

    Update Army Game.py

--- Army Game.py
+++ Army Game.py
@@ -48,7 +48,7 @@ print(ans)
 '''
 Luke is daydreaming in Math class. He has a sheet of graph paper with  rows and  columns, and he imagines that there is an army base in each cell for a total of  bases. He wants to drop supplies at strategic points on the sheet, marking each drop point with a red dot. If a base contains at least one package inside or on top of its border fence, then it's considered to be supplied. For example:
 
-image
+
 
 Given  and , what's the minimum number of packages that Luke must drop to supply all of his bases?
 
